2d559dbb7e6d2c34bf9783d2d69e3f2d8bbee852
Vitor <vitorlopesdesouza@gmail.com>
2018-11-16 22:56:06 -0200

    fix port

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const port = process.env.PORT || 5000;
+
 mongoose.connect(
   'mongodb://servicopredial:v6158678@ds157223.mlab.com:57223/ordem-servico-predial',
   {
@@ -24,6 +26,6 @@ app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('Server started on port 3000');
 });
